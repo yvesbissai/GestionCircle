7f5922477a617e58d2428fcac085eb66dcab2c29
yvesbissai <46777672+yvesbissai@users.noreply.github.com>
2023-06-17 17:36:34 +0200

    add projet test

---
+++ TestGestionCircle/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TestGestionCircle;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
